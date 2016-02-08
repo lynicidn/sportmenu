33b077d84ecfbf121d9e97f5f070e6ae3bc43783
lynicidn <lynicidn@gmail.com>
2016-02-08 05:18:13 +0300

    remove not needed files

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -7,7 +7,7 @@ use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
-use app\models\ContactForm;
+use app\models\RegistrationForm;
 
 class SiteController extends Controller
 {
@@ -40,16 +40,7 @@ class SiteController extends Controller
             'error' => [
                 'class' => 'yii\web\ErrorAction',
             ],
-            'captcha' => [
-                'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
-            ],
         ];
-    }
-
-    public function actionIndex()
-    {
-        return $this->render('index');
     }
 
     public function actionLogin()
@@ -74,21 +65,18 @@ class SiteController extends Controller
         return $this->goHome();
     }
 
-    public function actionContact()
+    public function actionRegistration($token = null)
     {
-        $model = new ContactForm();
-        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
-            Yii::$app->session->setFlash('contactFormSubmitted');
-
-            return $this->refresh();
+        if (!\Yii::$app->user->isGuest) {
+            return $this->goHome();
         }
-        return $this->render('contact', [
+
+        $model = new RegistrationForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        }
+        return $this->render('login', [
             'model' => $model,
         ]);
-    }
-
-    public function actionAbout()
-    {
-        return $this->render('about');
     }
 }
